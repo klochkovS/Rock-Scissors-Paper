a9ce89575dd3b6e5406bc7daf828403991cb31f5
klochkovS <fluurr@gmail.com>
2018-07-04 12:54:33 +0300

    123

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+//const server = require('http').Server(app);
+const io = require('socket.io')(app);
 const play = require('./lib/game');
 
 app.use(express.static('dist'));
